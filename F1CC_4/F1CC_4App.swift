849e7d8ae690964a16389cda4505bb73e8b6ab1b
DHJ1827 <davidjoyce1827@gmail.com>
2023-03-16 14:14:53 -0400

    Initial Commit

---
+++ F1CC_4/F1CC_4App.swift
@@ -0,0 +1,17 @@
+//
+//  F1CC_4App.swift
+//  F1CC_4
+//
+//  Created by David Joyce on 2023-03-16.
+//
+
+import SwiftUI
+
+@main
+struct F1CC_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
