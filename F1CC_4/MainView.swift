7810cd63cff1056948ec354d740f18713c8d647d
DHJ1827 <davidjoyce1827@gmail.com>
2023-03-31 11:27:30 -0400

    Working, next up Recs calls

--- F1CC_4/MainView.swift
+++ F1CC_4/MainView.swift
@@ -5,22 +5,27 @@
 //  Created by David Joyce on 2023-03-16.
 //
 // TO DO
-// Check Recs detail
-//    - design recs basic and details- try mid so that there's some possible upgrades to drivers and parts. Also fix spacing for various scenarios
-//    - fix string 1 for Driver and Part: line 42 and 54 for varied spacing depending on string lengths. Ok for min. Try with mid and max. NCo = 0?
-//    - add Team Score- use CR or PR sum for all ST drivers and parts
-//    - fix Driver display when PC > CL rows. right justify NBCa so that NCO is aligned
-//    - fix Part display spacing
-
+//
+// Fix
+// run, recs syas sainz and norris. then driver, recs, says Sainz and Sainz- no doubles allowed!
+// calcs, including teamScore are all mixed up!
 // Fix mid, min and max by changing 10% to 0, not 1
+// create app icon
+//
+// UI
+// fix string 1 for Driver and Part: line 42 and 54 for varied spacing depending on string lengths. Ok for min. Try with mid and max. NCo = 0?
+// fix Driver display when PC > CL rows. right justify NCa so that NCO is aligned
+// fix Part display spacing
+// add coloured driver fonts to basic recs to distinguish which driver
+// need consistent font and colours for headings and titles throughout
+//
+// Checks
 // Check on iPhone 13 and iPhone 8
 // Check AdMob
 // Check calcs
 // Check min, max, mid text files
 // Check against Android version
-// need consistent font and colours for headings and titles throughout
-//      - design recs basic. Add team score to Recs?
-// create app icon
+
 
 
 import SwiftUI
